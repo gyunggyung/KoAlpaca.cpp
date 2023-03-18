17e124703933ee96df14885bcbeaa7b40cd0f751
gyunggyung <newhiwoong@gmail.com>
2023-03-19 07:00:40 +0900

    feat: Edit main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
 #include <string>
+#include <transformers/transformers.h>
 #include "ggml.h"
 #include "utils.h"
-#include "chat.cpp"
 
-void simpleChat() {
-    std::string input;
-    std::cout << "Enter your message (type 'quit' to exit):" << std::endl;
+void useKoAlpacaModel() {
+    using namespace transformers;
 
-    while (true) {
-        std::getline(std::cin, input);
+    // Load the KoAlpaca model and tokenizer
+    auto model = GPT2Model::from_pretrained("beomi/KoAlpaca");
+    auto tokenizer = GPT2Tokenizer::from_pretrained("beomi/KoAlpaca");
 
-        if (input == "quit") {
-            break;
-        }
+    // Get user input
+    std::string input_text;
+    std::cout << "Enter your input text (type 'quit' to exit):" << std::endl;
+    std::getline(std::cin, input_text);
 
-        std::cout << "You said: " << input << std::endl;
-        std::cout << "Enter your message (type 'quit' to exit):" << std::endl;
+    while (input_text != "quit") {
+        // Tokenize the input text
+        auto tokens = tokenizer.encode(input_text);
+        tokens.input_ids.resize(tokens.input_ids.size() + 1);
+        tokens.attention_mask.resize(tokens.attention_mask.size() + 1);
+
+        // Generate a response using the model
+        auto outputs = model.generate(tokens);
+
+        // Decode the generated tokens and print the result
+        auto generated_text = tokenizer.decode(outputs[0]);
+        std::cout << "Generated text: " << generated_text << std::endl;
+
+        // Get the next input
+        std::cout << "Enter your input text (type 'quit' to exit):" << std::endl;
+        std::getline(std::cin, input_text);
     }
 }
 
 int main() {
-    std::cout << "Starting the chat application..." << std::endl;
+    std::cout << "Starting the KoAlpaca application..." << std::endl;
     
-    simpleChat();
+    useKoAlpacaModel();
     
-    std::cout << "Chat application finished." << std::endl;
+    std::cout << "KoAlpaca application finished." << std::endl;
     return 0;
 }
